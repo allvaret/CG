9a5f54e83450e1ff76197601f434983f735829a1
Alvaro <alvarodanko032@gmail.com>
2026-04-02 21:21:09 -0300

    Criação de translação

--- triang.py
+++ triang.py
@@ -7,6 +7,12 @@ LARGURA, ALTURA = 640, 480
 tela = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption("Cg - Transformações básicas")
 
+# Cores
+PRETO = (0, 0, 0)
+VERDE = (0, 255, 0)
+BRANCO = (255, 255, 255)
+LARANJA = (255, 165, 0)
+
 def armazena_pontos():
     print('Insira as coordenadas das vértices do polígono a ser desenhado:')
     print('Formato: x1 y1 x2 y2 ...')
@@ -16,7 +22,7 @@ def armazena_pontos():
     if len(valores) % 3 != 0:
         raise ValueError("Número ímpar de coordenadas. Cada ponto precisa de x e y.")
 
-    pontos = [[valores[i], valores[i+1]] for i in range(0, len(valores), 2)]
+    pontos = [[valores[i], valores[i + 1]] for i in range(0, len(valores), 2)]
 
     return pontos
 
@@ -34,23 +40,34 @@ def desenha_triangulo(tela, pontos, cor):
     # Aresta 2: ponto 1 → ponto 2
     # Aresta 3: ponto 2 → ponto 0  ← fecha o triângulo
     arestas = [
-        (pontos[0], pontos[1]),
-        (pontos[1], pontos[2]),
-        (pontos[2], pontos[0]),
+        [pontos[0], pontos[1]],
+        [pontos[1], pontos[2]],
+        [pontos[2], pontos[0]],
     ]
-
     for (x1, y1), (x2, y2) in arestas:
         desenha_reta_bresenham(tela, x1, y1, x2, y2, cor)
 
+    return arestas
 
-# Cores
-PRETO = (0, 0, 0)
-VERDE = (0, 255, 0)
-BRANCO = (255, 255, 255)
-LARANJA = (255, 165, 0)
+
+def translacao(tela, vet_deslocamento:list, pontos:list,cor=VERDE):
+    novos_nos = []
+    for x,y in pontos:
+        novo_x = x + vet_deslocamento[0]
+        novo_y = y + vet_deslocamento[1]
+
+        novos_nos.append([novo_x, novo_y])
+        print(novos_nos)
+    tela.fill(PRETO)
+    desenha_triangulo(tela, novos_nos, cor)
+    return novos_nos
 
 # Teste
-desenha_triangulo(tela, armazena_pontos(), VERDE)
+pontos = armazena_pontos()
+arestas = desenha_triangulo(tela, pontos, VERDE)
+
+vet_deslocacao = list(map(int, input("Digite o vetor de deslocação, formato x1 y2:\n").split()))
+trans = translacao(tela, vet_deslocacao , pontos)
 
 pygame.display.flip()
 
@@ -60,8 +77,8 @@ while rodando:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             rodando = False
+    pygame.display.flip()
 
 
 pygame.quit()
 sys.exit()
-
